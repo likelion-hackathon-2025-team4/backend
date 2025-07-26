a3112028e930a793b640f5a2033591dbebb6c15e
shinchaerin79 <shinchaerin79@gmail.com>
2025-07-27 04:44:06 +0900

    :tada: Start: Start New Project

---
+++ src/main/java/com/likelion/hackathon2025backend/Hackathon2025BackendApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.hackathon2025backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon2025BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon2025BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/hackathon2025backend/Hackathon2025BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.hackathon2025backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2025BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
